9324a1fc80a1e83450250d962e1514bd8f1a89e4
Gabriel Moraes <gabriel.moraesdasilva14@gmail.com>
2023-03-13 03:40:09 -0300

    feat: implements lazy loading

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,5 +1,15 @@
 import { Product } from "./SearchResults";
-import { memo } from "react";
+import { memo, useState } from "react";
+import dynamic from "next/dynamic";
+//import { AddProductToWishList } from "./AddProductToWishList";
+
+const AddProductToWishList = dynamic(
+  async () =>
+    import("./AddProductToWishList").then(
+      (module) => module.AddProductToWishList
+    ),
+  { loading: () => <span>loading...</span> }
+);
 
 type ProductItemProps = {
   product: Product;
@@ -7,12 +17,20 @@ type ProductItemProps = {
 };
 
 function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
   return (
     <div>
       {product.title} - <strong>{product.formattedPrice}</strong>
-      <button onClick={() => onAddToWishList(product.id)}>
+      <button onClick={() => setIsAddingToWishList(true)}>
         Add to wishlist
       </button>
+      {isAddingToWishList && (
+        <AddProductToWishList
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      )}
     </div>
   );
 }
